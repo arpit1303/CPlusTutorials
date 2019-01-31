78c40c7d775c7fddbc020dafe23d63ad52b25635
arpit1303 <arpit.1303@gmail.com>
2019-01-31 21:27:03 +1100

    This program is an example on how to use "IF" Statement.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Arpit SIngh on 2019-01-30.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a ;
+
+    cout << "Enter the number: ";
+    cin >> a ;
+
+    if (a > 0){
+
+        cout << "You have entered a positive number :" << a << endl;
+
+    }
+
+    cout << "This statement is always executed.";
+
+    return 0;
+}
