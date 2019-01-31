af76884359cf83d82faa6d4fd9a3d361ac37a1f5
arpit1303 <arpit.1303@gmail.com>
2019-01-31 21:41:01 +1100

    This program is an example on how to use "IF & ELSE" Statement. Also, the variable 'a' has been changed to 'num'.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Arpit SIngh on 2019-01-30.
+// Created by Arpit Singh on 2019-01-30.
 //
 
 #include <iostream>
@@ -7,15 +7,20 @@ using namespace std;
 
 int main(){
 
-    int a ;
+    int num ;
 
     cout << "Enter the number: ";
-    cin >> a ;
+    cin >> num ;
 
-    if (a > 0){
+    if (num > 0){
 
-        cout << "You have entered a positive number :" << a << endl;
+        cout << "You have entered a positive number : " << num << endl;
 
+    }
+
+    else {
+
+        cout << "You have entered a negative number : " << num << endl;
     }
 
     cout << "This statement is always executed.";
